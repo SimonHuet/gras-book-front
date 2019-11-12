402ba548a908698a01b89ea2f129bd5e19a85c65
Jeremy Chauvin <jeremie.chauvin@epsi.fr>
2019-11-12 08:58:46 +0000

    init router ui

---
+++ src/Utils/Router/Router.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Router, Switch, Route } from 'react-router'
+import Login from '../../Components/Login'
+
+export default () => <Router>
+    <Switch>
+        <Route exact path="/" component={Login}/>
+    </Switch>
+</Router>
