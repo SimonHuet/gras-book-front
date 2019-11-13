90e6b60c3c3d0eda6d4ddb9d3e0a18ab42d1a441
Simon Huet <huetsimon35+gitlab@gmail.com>
2019-11-13 11:47:04 +0100

    feat(): Created button and button story

---
+++ src/Components/Button/button.stories.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from './Button.js';
+
+export default { title: 'Button' };
+
+export const withText = () => 
+        <div style={{ padding: '10px' }}>
+            <Button small placeholder="Gras book" />
+            <br/>
+            <br/>
+            <Button medium placeholder="Gras book" />
+            <br/>
+            <br/>
+            <Button large placeholder="Gras book" />
+        </div>
