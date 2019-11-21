d5deaefa2306a74347faeb1e6ecd5aa058e072dc
Simon Huet <huetsimon35+gitlab@gmail.com>
2019-11-21 14:30:49 +0100

    fix(): fix router and add History

--- src/Utils/Router/Router.js
+++ src/Utils/Router/Router.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Router, Switch, Route } from 'react-router';
+<<<<<<< HEAD
 import Login from '../../Components/Login';
 
 export default () => <Router>
@@ -7,3 +8,15 @@ export default () => <Router>
         <Route exact path="/" component={Login}/>
     </Switch>
 </Router>;
+=======
+import Login from 'Components/Login';
+import { history } from 'Utils/History';
+
+export default () => <Router history={history}>
+    <main>
+        <Switch>
+            <Route exact path="/(login/|login)" component={Login} />
+        </Switch>
+    </main>
+</Router>;
+>>>>>>> fix(): fix router and add History
