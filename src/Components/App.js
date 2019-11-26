ef351e216c80d821cce9ed9a8fffbeca77407e32
Jeremy Chauvin <jeremie.chauvin@epsi.fr>
2019-11-26 23:06:19 +0100

    feat(): add keycloak

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Router from 'Utils/Router/Router';
 import { SuspenseWrapper } from 'Components/UI/utils/SuspenseWrapper';
+import Keycloak from 'keycloak-js';
+import { KeycloakProvider } from 'react-keycloak';
 
-const App = () => <div className="App container">
+const keycloak = new Keycloak()
+const initKeycloakProviderConfig = {onLoad: 'login-required'}
+const App = () => <KeycloakProvider keycloak={keycloak} initConfig={initKeycloakProviderConfig}><div className="App container">
   <SuspenseWrapper>
     <Router />
   </SuspenseWrapper>
-</div>;
+</div>
+</KeycloakProvider>;
 
 export default App;
--- src/Components/Pages/Login/index.js
+++ src/Components/Pages/Login/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { useKeycloak } from 'react-keycloak';
 
-export default () => { 
+export default () => {
     const { t } = useTranslation('Login');
+    const { keycloak } = useKeycloak();
     return (<>
-    <h1>{t('title')}</h1>
-</>);
+        <h1>{t('title')}</h1>
+        {!!keycloak.authenticated && (
+        <button type="button" onClick={() => keycloak.logout()}>
+          Logout
+        </button>
+      )}
+    </>);
 };
