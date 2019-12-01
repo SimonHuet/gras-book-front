3806472fe7e09d4f1a92c5bc2495066e492b2423
Simon Huet <huetsimon35+gitlab@gmail.com>
2019-12-01 22:56:19 +0100

    feat(): adding FetchBackend

--- src/Utils/fetchBackend.js
+++ src/Utils/fetchBackend.js
@@ -1,1 +1,36 @@
-export default () => 'fecthc';
+export default (baseUrl, path, options = {}) => {
+
+    options = {
+        ...options,
+        ...getHeaders(options.headers),
+    };
+
+    fetch(`${baseUrl}/${path}`, options)
+    .then(response =>
+        response.status < 400
+            ? resolveResponseAndBody(response)
+            : Promise.reject(response)
+    )
+    .then(([response, body]) => ({
+        status: response.status,
+        headers: response.headers,
+        body,
+    }));
+};
+
+
+const getHeaders = optionsHeaders => {
+    const token = localStorage.user;
+
+    return {
+        headers: {
+            ...optionsHeaders,
+            ...(token ? { Authorization: `Bearer ${token}` } : null),
+        },
+    };
+};
+
+const resolveResponseAndBody = response =>
+     response.headers.get('Content-Type') === 'application/json'
+        ? Promise.all([response, response.json()])
+        : Promise.all([response, response.text()]);
