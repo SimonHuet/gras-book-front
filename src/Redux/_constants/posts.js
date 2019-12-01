e065c82b55428955bc080e580b39a9ef2a47335e
Simon Huet <huetsimon35+gitlab@gmail.com>
2019-12-01 21:55:20 +0100

    feat(): adding reducer & constants

---
+++ src/Redux/_constants/posts.js
@@ -0,0 +1,5 @@
+export const postsConstants = {
+    FETCH_POSTS: 'grasbook/posts/FETCH_POSTS',
+    POSTS_FETCHED: 'grasbook/posts/POSTS_FETCHED',
+    POSTS_FETCH_ERROR:'grasbook/posts/POSTS_FETCH_ERROR',
+};
---
+++ src/Redux/_reducers/posts.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-param-reassign */
+import { postsConstants } from 'Redux/_constants/posts';
+
+export const INITIAL_STATE = {
+    posts: [],
+    isFetchingPosts: false,
+    isFetchError: false,
+};
+
+export const reducer = (oldSate = INITIAL_STATE, action = {}) => {
+    oldSate = oldSate || INITIAL_STATE;
+    action = action || {};
+
+    switch (action.type) {
+        case postsConstants.FETCH_POSTS:
+            return {
+                ...oldSate,
+                isFetchingPosts: true,
+            };
+        case postsConstants.POSTS_FETCHED:
+            return {
+                ...oldSate,
+                posts: action.posts,
+                isFetchingPosts: false,
+            };
+        case postsConstants.POSTS_FETCH_ERROR:
+            return {
+                ...oldSate,
+                isFetchingPosts: false,
+                isFetchError: true,
+            };
+        default:
+            return oldSate;
+    }
+};
