5a155c999b09b4ba8ef9946129fd68d8bcb78c9f
eddy <edd98102@gmail.com>
2019-12-02 23:44:24 +0100

    ...

--- src/Utils/Router/Router.js
+++ src/Utils/Router/Router.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Router, Switch, Route } from 'react-router-dom';
 import Login from 'Components/Pages/Login';
+import Groups from 'Components/Pages/Groups';
 import { history } from 'Utils/History';
 import NetworkError from 'Components/Pages/Errors/NetworkError';
 
-export default () => <Router history={history}>
+export default () => (
+  <Router history={history}>
     <main>
-        <Switch>
-            <Route exact path="/" component={ Login } />
-            <Route exact path="/network-error" component={ NetworkError } />
-        </Switch>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/network-error" component={NetworkError} />
+        <Route exact path="/Groups" component={Groups} />
+      </Switch>
     </main>
-</Router>;
+  </Router>
+);
