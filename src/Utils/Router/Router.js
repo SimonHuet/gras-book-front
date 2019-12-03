d3919e6ecdefc9e0a7b7977723e762e3d0f39d7c
Simon Huet <huetsimon35+gitlab@gmail.com>
2019-12-03 21:55:57 +0100

    feat(): add Profile route

--- src/Utils/Router/Router.js
+++ src/Utils/Router/Router.js
@@ -3,12 +3,14 @@ import { Router, Switch, Route } from 'react-router-dom';
 import Login from 'Components/Pages/Login';
 import { history } from 'Utils/History';
 import NetworkError from 'Components/Pages/Errors/NetworkError';
+import Profile from 'Components/Pages/Profile';
 
 export default () => <Router history={history}>
     <main>
         <Switch>
             <Route exact path="/" component={ Login } />
             <Route exact path="/network-error" component={ NetworkError } />
+            <Route exact path="/profile/{id}" component={ Profile } />
         </Switch>
     </main>
 </Router>;
