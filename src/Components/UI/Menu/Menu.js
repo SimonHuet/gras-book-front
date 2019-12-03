8e9273ea7ff00bbf1a8a4d121b2872b2600d4469
Yann DURAND <ydurand@4cad.fr>
2019-12-03 18:25:44 +0100

    WIP: work on menu comp

--- src/Components/UI/Menu/Menu.js
+++ src/Components/UI/Menu/Menu.js
@@ -6,53 +6,68 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import {
-  homeIcon,
-  profileIcon,
-  groupIcon,
-  notificationIcon,
-  messagesIcon,
-  settingsIcon
-} from '../../../Resources/MenuIcon';
+import HomeTwoToneIcon from '@material-ui/icons/HomeTwoTone';
+import PersonOutlineTwoToneIcon from '@material-ui/icons/PersonOutlineTwoTone';
+import GroupTwoToneIcon from '@material-ui/icons/GroupTwoTone';
+import NotificationsNoneTwoToneIcon from '@material-ui/icons/NotificationsNoneTwoTone';
+import ChatTwoToneIcon from '@material-ui/icons/ChatTwoTone';
+import SettingsTwoToneIcon from '@material-ui/icons/SettingsTwoTone';
 
 const useStyles = makeStyles({
   root: {
-    width: 230,
+    width: 440,
+    height: 390,
+    backgroundColor: '#bdbdbd',
   },
+  item: {
+    height: 46,
+    border: 0,
+    borderTop: 1,
+  },
+  icon: {
+    height: 40,
+    width: 40,
+  }
 });
 
-export default function TypographyMenu() {
-  const { t } = useTranslation('Login');
+export default () => {
+  const { t } = useTranslation('Menu');
   const classes = useStyles();
 
   return (
     <Paper className={classes.root}>
       <MenuList>
-        <MenuItem>
-          <img src={homeIcon} alt='home' />
+        <MenuItem className={classes.item}>
+          <HomeTwoToneIcon className={classes.icon}>
             <Typography variant="inherit">{t('menu.home')}</Typography>
+          </HomeTwoToneIcon>            
         </MenuItem>
-        <MenuItem>
-          <img src={profileIcon} alt='profile' />
+        <MenuItem className={classes.item}>
+          <PersonOutlineTwoToneIcon className={classes.icon}>
             <Typography variant="inherit">{t('menu.profile')}</Typography>
+          </PersonOutlineTwoToneIcon>            
         </MenuItem>
-        <MenuItem>
-          <img src={groupIcon} alt='group' />
+        <MenuItem className={classes.item}>
+          <GroupTwoToneIcon className={classes.icon}>
             <Typography variant="inherit">{t('menu.group')}</Typography>
+          </GroupTwoToneIcon>            
         </MenuItem>
-        <MenuItem>
-          <img src={notificationIcon} alt='notif' />
+        <MenuItem className={classes.item}>
+          <NotificationsNoneTwoToneIcon className={classes.icon}>
             <Typography variant="inherit">{t('menu.notification')}</Typography>
+          </NotificationsNoneTwoToneIcon>            
         </MenuItem>
-        <MenuItem>
-          <img src={messagesIcon} alt='msg' />
-            <Typography variant="inherit">{t('menu.message')}</Typography>
+        <MenuItem className={classes.item}>
+          <ChatTwoToneIcon className={classes.icon}>
+            <Typography variant="inherit">{t('menu.message')}</Typography>          
+          </ChatTwoToneIcon>
         </MenuItem>
-        <MenuItem>
-          <img src={settingsIcon} alt='settings' />
+        <MenuItem className={classes.item}>
+          <SettingsTwoToneIcon className={classes.icon}>
             <Typography variant="inherit">{t('menu.settings')}</Typography>
+          </SettingsTwoToneIcon>
         </MenuItem>
       </MenuList>
     </Paper>
   );
-}
+};
