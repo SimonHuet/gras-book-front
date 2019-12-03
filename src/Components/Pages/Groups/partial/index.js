e0d746ad0981ad3f6842cc8e518db85fe787ec2d
Eddy Cheval <eddy.cheval@odiwi.com>
2019-12-03 12:39:26 +0000

    feat(): redux for user et materiel ui

--- src/Components/Pages/Groups/partial/index.js
+++ src/Components/Pages/Groups/partial/index.js
@@ -1,3 +1,25 @@
 import form from './form';
+import { usersFetched, fetchUsers ,usersFetchError } from 'Redux/_actions/users';
+import { connectWithLifecycle } from 'react-lifecycle-component';
 
-export { form };
+const mapStateToProps = state =>({
+    users: state.group.users,
+    fetchError: state.group.FetchError
+});
+
+const mapDispatchToProps = dispatch => ({
+    componentDidMount: () => {
+        dispatch(fetchUsers());
+
+        fetchBackend(process.env.REACT_APP_USER_API, "routes")
+        .then(data => dispatch(usersFetched(data)))
+        .catch( err  => dispatch(
+            usersFetchError(err))
+        );
+    }
+});
+
+export default connectWithLifecycle(
+    mapStateToProps,
+    mapDispatchToProps,
+)(form);
