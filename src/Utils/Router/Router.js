5eab070586abb93a7e752d8d3cc9193e6e8eb6f9
Simon Huet <huetsimon35+gitlab@gmail.com>
2019-12-03 19:06:30 +0100

    refacto(): Menu

--- src/Utils/Router/Router.js
+++ src/Utils/Router/Router.js
@@ -3,12 +3,14 @@ import { Router, Switch, Route } from 'react-router-dom';
 import Login from 'Components/Pages/Login';
 import { history } from 'Utils/History';
 import NetworkError from 'Components/Pages/Errors/NetworkError';
+import Home from 'Components/Pages/Home/Home';
 
 export default () => <Router history={history}>
     <main>
         <Switch>
             <Route exact path="/" component={ Login } />
             <Route exact path="/network-error" component={ NetworkError } />
+            <Route exact path="/home" component={Home} />
         </Switch>
     </main>
 </Router>;
