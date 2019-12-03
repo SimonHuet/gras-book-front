f094410cec4af74a97ab47066a2be7d895fd08b3
Yann DURAND <ydurand@4cad.fr>
2019-12-03 08:44:05 +0100

    feat: add menu and icons (wip)

--- src/Components/UI/Menu/Menu.js
+++ src/Components/UI/Menu/Menu.js
@@ -1,104 +1,58 @@
+/* eslint-disable import/no-unresolved */
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import ClickAwayListener from '@material-ui/core/ClickAwayListener';
-import Grow from '@material-ui/core/Grow';
-import Paper from '@material-ui/core/Paper';
-import Popper from '@material-ui/core/Popper';
-import MenuItem from '@material-ui/core/MenuItem';
-import MenuList from '@material-ui/core/MenuList';
-import { makeStyles } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
+import MenuList from '@material-ui/core/MenuList';
+import MenuItem from '@material-ui/core/MenuItem';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import {
+  homeIcon,
+  profileIcon,
+  groupIcon,
+  notificationIcon,
+  messagesIcon,
+  settingsIcon
+} from '../../../Resources/MenuIcon';
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles({
   root: {
-    display: 'flex',
+    width: 230,
   },
-  paper: {
-    marginRight: theme.spacing(2),
-  },
-}));
+});
 
-export default function MenuListComposition() {
-    const { t } = useTranslation('Login');
+export default function TypographyMenu() {
+  const { t } = useTranslation('Login');
   const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
-  const anchorRef = React.useRef(null);
-
-  const handleToggle = () => {
-    setOpen(prevOpen => !prevOpen);
-  };
-
-  const handleClose = event => {
-    if (anchorRef.current && anchorRef.current.contains(event.target)) {
-      return;
-    }
-
-    setOpen(false);
-  };
-
-  function handleListKeyDown(event) {
-    if (event.key === 'Tab') {
-      event.preventDefault();
-      setOpen(false);
-    }
-  }
-
-  const prevOpen = React.useRef(open);
-  React.useEffect(() => {
-    if (prevOpen.current === true && open === false) {
-      anchorRef.current.focus();
-    }
-
-    prevOpen.current = open;
-  }, [open]);
 
   return (
-    <div className={classes.root}>
-      <Paper className={classes.paper}>
-        <MenuList>
-          <MenuItem><Typography component="h1" variant="h5">
-                        {t('auth.title')} 
-                    </Typography></MenuItem>
-          <MenuItem>My Profile</MenuItem>
-          <MenuItem>My groups</MenuItem>
-          <MenuItem>Notifications</MenuItem>
-          <MenuItem>Messages</MenuItem>
-          <MenuItem>Settings</MenuItem>
-        </MenuList>
-      </Paper>
-      <div>
-        <Button
-          ref={anchorRef}
-          aria-controls={open ? 'menu-list-grow' : undefined}
-          aria-haspopup="true"
-          onClick={handleToggle}
-        >
-          Toggle Menu Grow
-        </Button>
-        <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
-          {({ TransitionProps, placement }) => (
-            <Grow
-              // eslint-disable-next-line react/jsx-props-no-spreading
-              {...TransitionProps}
-              style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
-            >
-              <Paper>
-                <ClickAwayListener onClickAway={handleClose}>
-                  <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
-                    <MenuItem onClick={handleClose}>Home</MenuItem>
-                    <MenuItem onClick={handleClose}>My profile</MenuItem>
-                    <MenuItem onClick={handleClose}>My groups</MenuItem>
-                    <MenuItem onClick={handleClose}>Notifications</MenuItem>
-                    <MenuItem onClick={handleClose}>Messages</MenuItem>
-                    <MenuItem onClick={handleClose}>Settings</MenuItem>
-                  </MenuList>
-                </ClickAwayListener>
-              </Paper>
-            </Grow>
-          )}
-        </Popper>
-      </div>
-    </div>
+    <Paper className={classes.root}>
+      <MenuList>
+        <MenuItem>
+          <img src={homeIcon} alt='home' />
+            <Typography variant="inherit">{t('menu.home')}</Typography>
+        </MenuItem>
+        <MenuItem>
+          <img src={profileIcon} alt='profile' />
+            <Typography variant="inherit">{t('menu.profile')}</Typography>
+        </MenuItem>
+        <MenuItem>
+          <img src={groupIcon} alt='group' />
+            <Typography variant="inherit">{t('menu.group')}</Typography>
+        </MenuItem>
+        <MenuItem>
+          <img src={notificationIcon} alt='notif' />
+            <Typography variant="inherit">{t('menu.notification')}</Typography>
+        </MenuItem>
+        <MenuItem>
+          <img src={messagesIcon} alt='msg' />
+            <Typography variant="inherit">{t('menu.message')}</Typography>
+        </MenuItem>
+        <MenuItem>
+          <img src={settingsIcon} alt='settings' />
+            <Typography variant="inherit">{t('menu.settings')}</Typography>
+        </MenuItem>
+      </MenuList>
+    </Paper>
   );
 }
