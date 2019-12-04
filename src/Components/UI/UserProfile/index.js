721dda6ea77c355bc07508ff8ed8cf8949eea332
Simon Huet <huetsimon35+gitlab@gmail.com>
2019-12-04 22:17:38 +0100

    fix() unstable

--- src/Components/UI/UserProfile/index.js
+++ src/Components/UI/UserProfile/index.js
@@ -1,7 +1,6 @@
 import fetchBackend from 'Utils/fetchBackend';
 import { fetchUser, userFetched, userFetchError } from 'Redux/_actions/profile';
 import { connectWithLifecycle } from 'react-lifecycle-component';
-import { userService } from 'Utils/User.service';
 import UserProfile from './UserProfile';
 
 const mapStateToProps = state =>({
@@ -9,11 +8,11 @@ const mapStateToProps = state =>({
     userFetchError: state.profile.fetchError
 });
 
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = (dispatch, ownProps) => ({
     componentDidMount: () => {
         dispatch(fetchUser);
 
-        fetchBackend(process.env.REACT_APP_USER_API, `users/${userService.getConnectedUser()}`)
+        fetchBackend(process.env.REACT_APP_USER_API, `users/${ownProps.id}`)
         .then( user => dispatch(userFetched(user)))
         .catch(err => dispatch(userFetchError(err)));
     }
