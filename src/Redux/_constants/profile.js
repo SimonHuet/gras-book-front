12703d354705c005afe2b44b170486cd91ce6246
Yann DURAND <ydurand@4cad.fr>
2019-12-04 23:07:54 +0100

    feat: add users reducers

---
+++ src/Redux/_constants/profile.js
@@ -0,0 +1,5 @@
+export const profileConstants = {
+    FETCH_USER: 'grasbook/profile/FETCH_USER',
+    USER_FETCHED: 'grasbook/profile/USER_FETCHED',
+    USER_FETCH_ERROR: 'grasbook/profile/POSTS_FETCH_ERROR',
+};
