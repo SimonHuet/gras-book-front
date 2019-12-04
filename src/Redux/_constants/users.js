8a98479d7d49457c8850e7acf7aba3151b05e5b3
Yann DURAND <ydurand@4cad.fr>
2019-12-04 23:07:54 +0100

    feat: add users reducers

--- src/Redux/_constants/users.js
+++
@@ -1,5 +0,0 @@
-export const usersConstants = {
-  FETCH_USERS: 'grasbook/group/FETCH_USERS',
-  USERS_FETCHED: 'grasbook/group/USERS_FETCHED',
-  USERS_FETCH_ERROR: 'grasbook/group/USERS_FETCH_ERROR',
-};
