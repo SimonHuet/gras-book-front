5dbddcdd448d0e43f64f82f3b9192e5478d1665f
Simon Huet <huetsimon35+gitlab@gmail.com>
2019-12-08 11:34:27 +0100

    feat(): update redux impl

--- src/Redux/_constants/profile.js
+++ src/Redux/_constants/profile.js
@@ -2,4 +2,7 @@ export const profileConstants = {
     FETCH_USER: 'grasbook/profile/FETCH_USER',
     USER_FETCHED: 'grasbook/profile/USER_FETCHED',
     USER_FETCH_ERROR:'grasbook/profile/POSTS_FETCH_ERROR',
+    FETCH_USER_POSTS: 'grasbook/profile/FETCH_USER_POSTS',
+    USER_POSTS_FETCHED: 'grasbook/profile/USER_POSTS_FETCHED',
+    USER_POSTS_FETCH_ERROR: 'grasbook/profile/USER_POSTS_FETCH_ERROR',
 };
--- src/Redux/_reducers/index.js
+++ src/Redux/_reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
-import { reducer as post } from './posts';
+import { reducer as posts } from './posts';
 import { reducer as profile } from './profile';
 
 export const reducer = combineReducers({
-    post,
+    posts,
     profile
 });
