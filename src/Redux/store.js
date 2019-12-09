9ad90870ab1129a4ccec56e1cbf57b431fedcd17
Simon Huet <huetsimon35+gitlab@gmail.com>
2019-12-09 08:23:11 +0100

    fix() pipeline

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-console */
-import { createStore, compose, applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk';
+import { createStore /* , compose, applyMiddleware */} from 'redux';
+// import thunkMiddleware from 'redux-thunk';
 import { reducer } from 'Redux/_reducers';
-import { connectRouter, routerMiddleware } from 'connected-react-router';
+import { connectRouter /* , routerMiddleware */ } from 'connected-react-router';
 import { history } from 'Utils/History';
 
 // eslint-disable-next-line no-shadow
