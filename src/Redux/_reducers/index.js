6d2047c978096459d2683538212c9f176f117b0b
Yann DURAND <ydurand@4cad.fr>
2019-12-09 15:46:03 +0100

    fix: fix prob after merge branch

--- src/Redux/_reducers/index.js
+++ src/Redux/_reducers/index.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux';
 import { reducer as posts } from './posts';
-import { reducer as users } from './users';
 import { reducer as groupUsers } from './groupUsers';
 import { reducer as groups } from './groups';
 import { reducer as messages } from './messages';
@@ -8,7 +7,6 @@ import { reducer as profile } from './profile';
 
 export const reducer = combineReducers({
   posts,
-  users,
   groups,
   messages,
   groupUsers,
